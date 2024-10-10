a7c82689ebac710fc94dc968f3b968dc76329b02
dninicat <daniele.ninicato@kindengezin.be>
2024-10-10 20:21:20 +0200

    fix converting to pdf

--- src/main/java/org/free/DocXTemplateReplacer.java
+++ src/main/java/org/free/DocXTemplateReplacer.java
@@ -1,35 +1,48 @@
 package org.free;
 
-import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
-import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.commons.compress.archivers.examples.Archiver;
 import org.apache.commons.compress.archivers.examples.Expander;
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.FopFactoryBuilder;
+import org.docx4j.Docx4J;
+import org.docx4j.convert.out.FOSettings;
+import org.docx4j.convert.out.fo.renderers.FORendererApacheFOP;
+import org.docx4j.fonts.BestMatchingMapper;
+import org.docx4j.fonts.Mapper;
+import org.docx4j.fonts.PhysicalFonts;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.docx4j.XmlUtils.marshaltoString;
+import static org.docx4j.jaxb.Context.getFopConfigContext;
 
 public class DocXTemplateReplacer {
+
+  private static Logger log = LoggerFactory.getLogger(DocXTemplateReplacer.class);
+
   public static void main(String[] args) throws Exception {
     var template = Paths.get("C:\\Development\\own\\spire-free\\src\\main\\resources\\template.docx");
     var outputFile = Paths.get("C:\\Development\\own\\spire-free\\src\\main\\resources\\saved.docx");
 
     mailMerge(template, outputFile);
-    System.out.println("Done");
+    log.info("Done");
   }
 
   public static File mailMerge(Path template, Path outputFile) throws Exception {
-    var tempFolder = Paths.get("C:\\Development\\temp\\merge");
+    var tempFolder = Paths.get("C:\\Development\\own\\spire-free\\src\\main\\resources\\merge");
 
     extract(template, tempFolder);
 
@@ -70,14 +83,29 @@ public class DocXTemplateReplacer {
   }
 
   public static void convertToPdf(File outputPdf) throws Exception {
-    try {
-      InputStream doc = new FileInputStream(outputPdf);
-      XWPFDocument document = new XWPFDocument(doc);
-      PdfOptions options = PdfOptions.create();
-      OutputStream out = new FileOutputStream(new File("C:\\Development\\own\\spire-free\\src\\main\\resources\\saved.pdf"));
-      PdfConverter.getInstance().convert(document, out, options);
-    } catch (IOException ex) {
-      System.out.println(ex.getMessage());
-    }
+    PhysicalFonts.addPhysicalFont(Objects.requireNonNull(DocXTemplateReplacer.class.getClassLoader().getResource("fonts/FlandersArtSans_Bold/FlandersArtSans-Bold.otf")).toURI());
+    PhysicalFonts.addPhysicalFont(Objects.requireNonNull(DocXTemplateReplacer.class.getClassLoader().getResource("fonts/FlandersArtSans_Regular/FlandersArtSans-Regular.otf")).toURI());
+    PhysicalFonts.addPhysicalFont(Objects.requireNonNull(DocXTemplateReplacer.class.getClassLoader().getResource("fonts/FlandersArtSerifMedium/FlandersArtSerif-Medium.ttf")).toURI());
+    PhysicalFonts.addPhysicalFont(Objects.requireNonNull(DocXTemplateReplacer.class.getClassLoader().getResource("fonts/FlandersArtSans_Italic/FlandersArtSans-Italic.otf")).toURI());
+
+    // Load the .docx file
+    WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(outputPdf);
+
+    Mapper fontMapper = new BestMatchingMapper();
+    wordMLPackage.setFontMapper(fontMapper);
+
+    FOSettings foSettings = new FOSettings(wordMLPackage);
+    log.info(marshaltoString(foSettings.getFopConfig(), getFopConfigContext()));
+    FopFactoryBuilder fopFactoryBuilder = FORendererApacheFOP.getFopFactoryBuilder(foSettings);
+    FopFactory fopFactory = fopFactoryBuilder.build();
+    FOUserAgent foUserAgent = FORendererApacheFOP.getFOUserAgent(foSettings, fopFactory);
+
+    OutputStream os = new FileOutputStream(new File("C:\\Development\\own\\spire-free\\src\\main\\resources\\saved.pdf"));
+
+    Docx4J.toFO(foSettings, os, Docx4J.FLAG_EXPORT_PREFER_XSL);
+
+    os.close();
+
+    log.info("PDF file generated successfully.");
   }
 }
