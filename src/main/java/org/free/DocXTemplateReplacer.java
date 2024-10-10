d6a42e6f7340fb9e85d6da2fade9e82424f1ddf4
dninicat <daniele.ninicato@kindengezin.be>
2024-10-10 20:22:08 +0200

    fix converting to pdf

--- src/main/java/org/free/DocXTemplateReplacer.java
+++ src/main/java/org/free/DocXTemplateReplacer.java
@@ -38,7 +38,7 @@ public class DocXTemplateReplacer {
     var outputFile = Paths.get("C:\\Development\\own\\spire-free\\src\\main\\resources\\saved.docx");
 
     mailMerge(template, outputFile);
-    log.info("Done, can i push now?");
+    log.info("Done");
   }
 
   public static File mailMerge(Path template, Path outputFile) throws Exception {
